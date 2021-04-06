0b66f73cf825cef29c7fffd994ef8904f5094fd2
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-06 01:57:12 +0100

    added api documentation for get chapters by courseId

--- src/routes/chapters.routes.js
+++ src/routes/chapters.routes.js
@@ -4,7 +4,7 @@ const authenticateToken = require("../middlewares/authenticateToken");
 
 router.get("/", chaptersController.getAllChapters);
 router.get("/:chapterId", chaptersController.getChapterById);
-router.get("/bycourse/:courseId", chaptersController.getChaptersByCourse);
+router.get("/course/:courseId", chaptersController.getChaptersByCourse);
 
 module.exports = router;
 
@@ -38,6 +38,29 @@ module.exports = router;
  *           type: string
  *         required: true
  *         description: The Chapter id
+ *     responses:
+ *       "400":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Chapter'
+*/
+
+/**
+ * @swagger
+ *  /chapters/course/{courseId}:
+ *   get:
+ *     summary: Returns the chapters that belong to the course with courseId
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Course id
  *     responses:
  *       "400":
  *         description: Error
