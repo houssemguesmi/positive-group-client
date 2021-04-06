f422c959904f4238b8e66aaa55cae37925797e79
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-06 13:39:33 +0100

    added documentation for course routes

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -4,5 +4,63 @@ const authenticateToken = require("../middlewares/authenticateToken");
 
 // Retrieving existing Courses
 router.get("/", coursesController.getAllCourses);
+router.get("/:coursesId", coursesController.getCourseById)
 
 module.exports = router;
+
+/**
+ * @swagger
+ *  /courses:
+ *   get:
+ *     summary: Returns the full list of courses
+ *     responses:
+ *       "500":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Course'
+*/
+
+/**
+ * @swagger
+ *  /courses/{coursesId}:
+ *   get:
+ *     summary: Returns the full list of courses
+ *     responses:
+ *       "500":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Course'
+*/
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Course:
+ *        type: object
+ *        properties:
+ *          _id:
+ *            type: string
+ *          name:
+ *            type: string
+ *          description:
+ *            type: string
+ *          duration:
+ *            type: number
+ *          chapters:
+ *            type: array
+ *            items:
+ *              type: string
+*/
