b009875cb9ae8ead97f3e773766a4dac1d07e5df
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-06 02:45:36 +0100

    added request handler for /api/categories/:categoryId and documentation for categories routes

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -2,12 +2,25 @@ const Category = require("../models/Category");
 const repository = require("../repositories/base.repository");
 
 module.exports = {
+
   getAllCategories: async (req, res) => {
     try {
       const categories = await repository.findAll(Category);
       res.status(200).send(categories);
     } catch (e) {
-      console.error(e);
+      console.error(e)
+      res.status(400).send("Error")
     }
   },
+
+  getCategoryById: async (req, res) => {
+    try {
+      const category = await repository.findOneById(req.params.categoryId, Category);
+      res.status(200).send(category)
+    } catch (e) {
+      console.error(e)
+      res.status(400).send("Error")
+    }
+  }
+
 };
--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -4,5 +4,61 @@ const authenticateToken = require("../middlewares/authenticateToken");
 
 // Retrieving existing Categories
 router.get("/", categoriesController.getAllCategories);
+router.get("/:categoryId", categoriesController.getCategoryById)
 
 module.exports = router;
+
+/**
+ * @swagger
+ *  /categories:
+ *   get:
+ *     summary: Returns the full list of categories
+ *     responses:
+ *       "400":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Category'
+*/
+
+/**
+ * @swagger
+ *  /categories/{categoryId}:
+ *   get:
+ *     summary: Returns the full list of categories
+ *     responses:
+ *       "400":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Category'
+*/
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Category:
+ *        type: object
+ *        properties:
+ *          _id:
+ *            type: string
+ *          name:
+ *            type: string
+ *          description:
+ *            type: string
+ *          courses:
+ *            type: array
+ *            items:
+ *              type: string
+*/
