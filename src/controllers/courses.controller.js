f82f88a24f14147deb4578d5c7020aefeea537b7
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-06 13:44:47 +0100

    handled get course by Id request

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -2,19 +2,31 @@ const Course = require("../models/Course");
 const repository = require("../repositories/base.repository");
 
 module.exports = {
+
   getAllCourses: async (req, res) => {
     try {
       const courses = await repository.findAll(Course);
       res.status(200).send(courses);
     } catch (e) {
       console.error(e);
+      res.status(500).send(e)
+    }
+  },
+
+  getCourseById: async (req, res) => {
+    try {
+      const course = await repository.findOneById(req.params.courseId, Course)
+      res.status(200).send(course)
+    } catch (e) {
+      console.error(e);
+      res.status(500).send(e)
     }
   },
 
   unlockCourse: async (req, res) => {
     try {
     } catch (e) {
-      res.status(400).error("Internal Error");
+      res.status(500).error("Internal Error");
     }
   },
 };
