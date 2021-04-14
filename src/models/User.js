953743e47f72d094e398989b12b243f8cab1b763
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-14 19:25:39 +0100

    refactored user schema

--- src/models/User.js
+++ src/models/User.js
@@ -2,12 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
+  firstName: { type: String, unique: trim },
+  lastName: { type: String, unique: trim },
   birthDate: Date,
-  gender: String,
-  job: String,
-  email: { type: String, unique: true, required: true },
+  gender: { type: String, enum: ['male', 'female', null], default: null },
+  job: { type: String, unique: trim },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid email');
+      }
+    },
+  },
   password: { type: String, unique: true },
   isActivated: { type: Boolean, default: false },
   invitees: { type: Array, default: [] },
