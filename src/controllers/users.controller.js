c26e484ef23ad2b64ddae3ec338d3993cb5d67f7
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-16 15:13:41 +0100

    added request course method to users controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -133,6 +133,19 @@ module.exports = {
     }
   },
 
+  requestCourse: async (req, res) => {
+    try {
+      let userId = req.params.userId;
+      let courseId = req.params.courseId;
+      await User.findByIdAndUpdate(userId, { $push: { requests: courseId } })
+      res.status(200).send("Request Created")
+    } catch (error) {
+      res.status(500).error(error)
+    }
+  }
+
+
+
   // getBonus: async (req, res) => {
 
   // }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -10,6 +10,8 @@ router.post("/activate-account/:userId", usersController.activateAccount)
 router.get("/:token", usersController.getUserByToken);
 router.put("/:userId", usersController.updateUser);
 
+router.post("/:userId/request/:courseId", usersController.requestCourse)
+
 router.post("/:userId/:courseId", usersController.unlockCourse)
 
 module.exports = router;
