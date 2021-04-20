2bee6c638d44c735d8e37e6c2324a75a258998dd
Ikdemm <ikdem.benmbarek@aiesec.net>
2021-04-20 17:32:59 +0100

    implemented unlcok course with code and added documentation for courses routes

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -1,5 +1,7 @@
 const Course = require("../models/Course");
+const User = require("../models/User");
 const repository = require("../repositories/base.repository");
+const CourseCode = require("../models/CourseCode")
 
 module.exports = {
 
@@ -25,8 +27,28 @@ module.exports = {
 
   unlockCourse: async (req, res) => {
     try {
-    } catch (e) {
-      res.status(500).error("Internal Error");
+      let userId = req.body.userId;
+      let courseId = req.params.courseId;
+
+      let courseCode = CourseCode.findOne({ code: req.body.code })
+
+      // Checking if the code exists
+      if (!courseCode) {
+        res.status(403).send("Code does not exist")
+      } else if (courseCode.course != courseId) {
+        res.status(405).send("Code does not exist on this course")
+      }
+
+      // Adding the code to the codes list of the user
+      User.findByIdAndUpdate(userId, { $push: { courses: courseId } })
+
+      // Marking the code as used
+      CourseCode.findOneAndUpdate({ code: req.body.code }, { usedOn: Date.now, usedBy: userId })
+      res.status(200).send("Success")
+
+    } catch (error) {
+      console.error(error)
+      res.status(500).send(error);
     }
   },
 };
---
+++ src/routes/courses.routes.js
@@ -0,0 +1,138 @@
+const router = require("express").Router();
+const coursesController = require("../controllers/courses.controller");
+const authenticateToken = require("../middlewares/authenticateToken");
+const Course = require("../models/Course");
+
+// Retrieving existing Courses
+router.get("/", coursesController.getAllCourses);
+router.get("/:courseId", coursesController.getCourseById)
+router.post("/:courseId", coursesController.getCourseById)
+router.post("/unlock/:courseId", coursesController.unlockCourse)
+
+module.exports = router;
+
+/**
+ * @swagger
+ *  /courses:
+ *   get:
+ *     tags:
+ *       - courses
+ *     summary: Returns the full list of courses
+ *     responses:
+ *       "500":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Course'
+*/
+
+/**
+ * @swagger
+ *  /courses/unlock/:courseId:
+ *   post:
+ *     tags:
+ *       - courses
+ *     summary: Returns the full list of courses
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: id of the course
+ *
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *                parameters:
+ *                  userId:
+ *                    type: string
+ *                    description: the id of the user
+ *                  code:
+ *                    type: string
+ *                    description: the activation code of the course
+ *
+ *          application/x-www-form-urlencoded:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                payload:
+ *                  type: object
+ *                    parameters:
+ *                      userId:
+ *                        type: string
+ *                        description: the id of the user
+ *                      code:
+ *                        type: string
+ *                        description: the activation code of the course
+ *            encoding:
+ *              payload:
+ *                contentType: application/json
+ *
+ *
+ *     responses:
+ *       "500":
+ *         description: Error
+ *       "405":
+ *         description: Code does not exist
+ *       "403":
+ *         description: Code does not exist on that course
+ *       "200":
+ *         description: Success
+*/
+
+/**
+ * @swagger
+ *  /courses/{courseId}:
+ *   get:
+ *     tags:
+ *       - courses
+ *     summary: Returns the full list of courses
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: id of the course
+ *     responses:
+ *       "500":
+ *         description: Error
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Course'
+*/
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Course:
+ *        type: object
+ *        properties:
+ *          _id:
+ *            type: string
+ *          name:
+ *            type: string
+ *          description:
+ *            type: string
+ *          duration:
+ *            type: number
+ *          chapters:
+ *            type: array
+ *            items:
+ *              type: string
+*/
